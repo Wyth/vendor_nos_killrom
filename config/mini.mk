4a3dc081723538bbad577f8e09135aa8532e100b
dhacker29 <davidhackerdvm@gmail.com>
2012-12-28 07:12:57 -0600

    Use smaller audio set for xoom

--- config/mini.mk
+++ config/mini.mk
@@ -10,15 +10,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.performance.tuning=1 \
     persist.service.swiqi.enable=1 \
     video.accelerate.hw=1 \
-    ro.ril.enable.3g.prefix=1 \
-    ro.ril.hep=1 \
-    ro.ril.hsxpa=3 \
-    ro.ril.enable.dtm=1 \
-    ro.ril.gprsclass=12 \
-    ro.ril.hsdpa.category=28 \
-    ro.ril.enable.a53=1 \
-    ro.ril.enable.3g.prefix=1 \
-    ro.ril.hsupa.category=7 \
     persist.sys.purgeable_assets=1 \
     pm.sleep_mode=1 \
     ro.home_app_adj=1 \
@@ -37,10 +28,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.verify-bytecode=false \
     dalvik.vm.dexopt-flags=v=n,o=v \
     ro.dalvik.vm.checkjni=0 \
-    debug.composition.type=gpu \
     ro.max.fling_velocity=15000 \
     ro.min.fling_velocity=8000 \
-    ro.config.hw_fast_dormancy=1 \
     net.dns1=8.8.4.4 \
     net.dns2=8.8.8.8 \
     net.ppp0.dns1=8.8.8.8 \
@@ -54,23 +43,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.tcp.buffersize.lte=524288,1048576,2097152,524288,1048576,2097152 \
     net.tcp.buffersize.hsdpa=6144,87380,1048576,6144,87380,1048576
 
-ifeq ($(TARGET_PRODUCT),full_grouper)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=EazyE.ogg \
-    ro.config.notification_sound=Nobody-Move.ogg
-endif
-
-# Bring in all audio files
+# Base audio files
 include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
-# Include KILL audio files
-include vendor/nos/killrom/config/kill_audio.mk
-
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += \
 vendor/nos/killrom/overlay/common
@@ -83,21 +57,6 @@ PRODUCT_COPY_FILES += \
     vendor/nos/killrom/prebuilt/system/etc/init.d/01killbomb:system/etc/init.d/01killbomb \
     vendor/nos/killrom/prebuilt/system/etc/init.d/91killrzipalign:system/etc/init.d/91killrzialign \
     vendor/nos/killrom/prebuilt/system/etc/init.d/97killrtcp:system/etc/init.d/97killrtcp
-
-ifeq ($(TARGET_PRODUCT),killr_mako)
-PRODUCT_COPY_FILES += \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/02killrkernel:system/etc/init.d/02killrkernel \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/05killrfs:system/etc/init.d/05killrfs \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/99killrsystem:system/etc/init.d/99killrsystem
-endif
-
-ifeq ($(TARGET_PRODUCT),full_grouper)
-PRODUCT_COPY_FILES += \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/02killrkernel:system/etc/init.d/02killrkernelgrouper \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/03killrgovgrouper:system/etc/init.d/03killrgovgrouper \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/05killrfs:system/etc/init.d/05killrfsgrouper \
-    vendor/nos/killrom/prebuilt/system/etc/init.d/99killrsystem:system/etc/init.d/99killrsystemgrouper
-endif
 
 # sysinit and sysctl support
 PRODUCT_COPY_FILES += \
@@ -118,7 +77,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/nos/killrom/prebuilt/system/xbin/zipalign:system/xbin/zipalign \
     vendor/nos/killrom/prebuilt/system/xbin/sqlite3:system/xbin/sqlite3
-
-# Bootanimation murdr
-PRODUCT_COPY_FILES += \
-    vendor/nos/killrom/prebuilt/system/media/bootanimation.zip:system/media/bootanimation.zip
