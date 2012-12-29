4ceb71a8aa8cb9a32057231a8b68d649a589aba3
dhacker29 <davidhackerdvm@gmail.com>
2012-12-28 22:05:13 -0600

    Need some ui sounds on xoom

--- config/mini.mk
+++ config/mini.mk
@@ -1,6 +1,5 @@
 PRODUCT_BRAND ?= killrom
 
-
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Cobalt.ogg \
@@ -8,6 +7,27 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Base audio files
 include frameworks/base/data/sounds/AudioPackageElements.mk
+
+LOCAL_PATH:= frameworks/base/data/sounds
+
+# Effects
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/effects/ogg/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
+	$(LOCAL_PATH)/effects/ogg/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
+	$(LOCAL_PATH)/effects/ogg/Dock.ogg:system/media/audio/ui/Dock.ogg \
+	$(LOCAL_PATH)/effects/ogg/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
+	$(LOCAL_PATH)/effects/ogg/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
+	$(LOCAL_PATH)/effects/ogg/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
+	$(LOCAL_PATH)/effects/ogg/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
+	$(LOCAL_PATH)/effects/ogg/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
+	$(LOCAL_PATH)/effects/ogg/Lock.ogg:system/media/audio/ui/Lock.ogg \
+	$(LOCAL_PATH)/effects/ogg/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
+	$(LOCAL_PATH)/effects/ogg/Media_Volume.ogg:system/media/audio/ui/Media_Volume.ogg \
+	$(LOCAL_PATH)/effects/ogg/Undock.ogg:system/media/audio/ui/Undock.ogg \
+	$(LOCAL_PATH)/effects/ogg/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
+	$(LOCAL_PATH)/effects/ogg/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
+	$(LOCAL_PATH)/effects/ogg/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
+	$(LOCAL_PATH)/effects/ogg/VolumeIncremental.ogg:system/media/audio/ui/VolumeIncremental.ogg \
 
 PRODUCT_PACKAGE_OVERLAYS += \
 vendor/nos/killrom/overlay/common
