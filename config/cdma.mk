e64c17bcbc408d86991b8ce6ece8fec4e0b03cba
Jason Theory <killdroidhack@gmail.com>
2013-01-08 12:49:38 -0500

    Update config/cdma.mk
    Derp

--- config/cdma.mk
+++ config/cdma.mk
@@ -1,3 +1,3 @@
 # CDMA APN list
 PRODUCT_COPY_FILES += \
-    vendor/nos/killrom/system/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+    vendor/nos/killrom/prebuilt/system/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
